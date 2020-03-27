af1adcd2fa531351e99b71dbf014adfd680f45a0
Scottiep79 <sapullen79@yahoo.com>
2020-03-27 09:51:12 +0000

    made pyramid

--- mario.js
+++ mario.js
@@ -1,7 +1,5 @@
-printPyramid(5)
-function firstProgram(){
-  document.getElementById("pyramid").innerHTML = x;
-}
+printPyramid(10)
+
 
 
 
@@ -9,16 +7,17 @@ function printPyramid(height){
   for(let num = 0; num < height; num++){ 
     let space = "";
     for(let spaces = height - num; spaces > 1; spaces--){
-      space += " ";
+      space += ".";
     }
     for(let hash = 0; hash < num + 2; hash++){
       space += "#";
     }
-    console.log(space)
-
+    //console.log(space)
+    let newnode = document.createElement('p');
+    newnode.innerHTML = space;
+    document.getElementById('pyramid').appendChild(newnode);
 
 
 
   }
 }; 
-x = printPyramid();
